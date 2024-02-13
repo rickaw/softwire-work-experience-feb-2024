c2cef00236e01211a4f44f173b4089c9dee8689a
Dimi <dimi08grazhd@icloud.com>
2024-02-13 14:15:15 +0000

    filled boxes

--- src/game.js
+++ src/game.js
@@ -1,14 +1,14 @@
 var c = document.getElementById("canvas");
 var line = c.getContext("2d");
-//line.strokeStyle='rgba(0,0,0,0.2)'
+//line.strokeStyle='rgba(0,0,0,0.2)' -- > dont work 
 
+//for loops for drawing the grid 
 for(let i = 40; i<400; i+=40) {
     line.moveTo(i,0);
     line.lineTo(i,800);
     line.stroke();
     console.log(i)
 }
-
 for(let i = 40; i<800; i+=40) {
     line.moveTo(0,i);
     line.lineTo(400,i);
@@ -16,7 +16,18 @@ for(let i = 40; i<800; i+=40) {
     console.log(i)
 }
 
-const canvas = document.getElementById("canvas");
-const box = canvas.getContext("2d");
-box.fillStyle = "red";
-box.fillRect(40, 80, 40, 40);
+
+
+function fillBoxes(x, y) {
+    const canvas = document.getElementById("canvas");
+    const box = canvas.getContext("2d");
+    box.fillStyle = "red";
+    box.fillRect(y, x, 40, 40);
+}
+
+//calling the function above to fill boxes 
+fillBoxes(40,80)
+fillBoxes(120,160)
+fillBoxes(320,40)
+fillBoxes(400,400)
+
