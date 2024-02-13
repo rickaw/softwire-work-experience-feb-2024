19b983d399f4b16070c7ed1ccc45526d198bddcf
Dimi <dimi08grazhd@icloud.com>
2024-02-13 11:57:23 +0000

    adding grid lines

--- src/game.js
+++ src/game.js
@@ -8,3 +8,10 @@ for(let i = 40; i<400; i+=40) {
     line.stroke();
     console.log(i)
 }
+
+for(let i = 40; i<800; i+=40) {
+    line.moveTo(0,i);
+    line.lineTo(400,i);
+    line.stroke();
+    console.log(i)
+}
