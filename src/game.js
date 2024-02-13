195bb66ac041fcc3b06c2b12cd90d82ca84892b5
Dimi <dimi08grazhd@icloud.com>
2024-02-13 13:56:56 +0000

    filling in one box

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 var c = document.getElementById("canvas");
 var line = c.getContext("2d");
-
+//line.strokeStyle='rgba(0,0,0,0.2)'
 
 for(let i = 40; i<400; i+=40) {
     line.moveTo(i,0);
@@ -15,3 +15,8 @@ for(let i = 40; i<800; i+=40) {
     line.stroke();
     console.log(i)
 }
+
+const canvas = document.getElementById("canvas");
+const box = canvas.getContext("2d");
+box.fillStyle = "red";
+box.fillRect(40, 80, 40, 40);
