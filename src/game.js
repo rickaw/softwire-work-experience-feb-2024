8eb0be7d2eb728427af00c9743ebfed8a3d4fb9a
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2024-02-14 18:42:53 +0000

    Clean up

--- src/game.js
+++ src/game.js
@@ -1,113 +1,88 @@
-var c = document.getElementById("canvas");
-var line = c.getContext("2d");
-//line.strokeStyle='rgba(0,0,0,0.2)' -- > dont work 
+import { jBlock } from "./blocks.js";
 
-//for loops for drawing the grid 
-for(let i = 40; i<400; i+=40) {
-    line.moveTo(i,0);
-    line.lineTo(i,800);
-    line.stroke();
-    console.log(i)
-}
-for(let i = 40; i<800; i+=40) {
-    line.moveTo(0,i);
-    line.lineTo(400,i);
-    line.stroke();
-    console.log(i)
-}
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-function fillBox(row, column,colour) {
-    const canvas = document.getElementById("canvas");
-    const box = canvas.getContext("2d");
-    box.fillStyle = colour;
-    box.fillRect(column * 40 + 1, row * 40 + 1, 38, 38);
-}
-
-const jBlock = [
-    [0, 0, 1, 0],
-    [0, 0, 1, 0],
-    [0, 1, 1, 0],
-    [0, 0, 0, 0]
-  ]
-  
-  const iBlock = [
-    [0, 0, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 0],
-  ]
-
-
+drawGameBoardGrid()
+drawGridForNextBlock()
 
 window.addEventListener("keydown", handleKeyboardEvent)
 
-function handleKeyboardEvent(event) {
-  if (event.key === "ArrowUp") {
-    console.log("Up arrow pressed")
-  }
-  else if(event.key === "ArrowDown"){
-    console.log("Down arrow pressed")
-  }
-  else if(event.key ==="ArrowLeft"){
-    console.log("Left arrow pressed")
-  }
-  else if(event.key=== "ArrowRight"){
-    console.log("Right arrow pressed")
-  }
-
-}
-line.strokeStyle='rgba(0,0,0,3)' 
-
-//for loops for drawing the smaller grid 
-for(let i= 40; i<160; i+=40) {
-    line.moveTo(i,0);
-    line.lineTo(i,160);
-    line.stroke();
-    console.log(i)
-}
-for(let i = 40; i<160; i+=40) {
-    line.moveTo(0,i);
-    line.lineTo(160,i);
-    line.stroke();
-    console.log(i)
+const currentBlock = {
+  shape: jBlock, 
+  position: [3,3],
+  colour:'red',
 }
 
-function drawBlock(typeBlock,position,colour){
-  typeBlock.forEach(function (row,rowIndex){
+drawBlock(currentBlock.shape, currentBlock.position, currentBlock.colour)
+
+setInterval(() => {
+  moveCurrentBlockDown()
+}, 1000);
+
+function drawGameBoardGrid() {
+  for(let i = 40; i<400; i+=40) {
+    ctx.moveTo(i,0);
+    ctx.lineTo(i,800);
+    ctx.stroke();
+  }
+
+  for(let i = 40; i<800; i+=40) {
+    ctx.moveTo(0,i);
+    ctx.lineTo(400,i);
+    ctx.stroke();
+  }
+}
+
+function drawGridForNextBlock() {
+  ctx.strokeStyle='rgba(0,0,0,0.3)'
+
+  for(let i= 40; i<160; i+=40) {
+    ctx.moveTo(i,0);
+    ctx.lineTo(i,160);
+    ctx.stroke();
+  }
+
+  for(let i = 40; i<160; i+=40) {
+    ctx.moveTo(0,i);
+    ctx.lineTo(160,i);
+    ctx.stroke();
+  }
+}
+
+function fillBox(row, column,colour) {
+  ctx.fillStyle = colour;
+  ctx.fillRect(column * 40 + 1, row * 40 + 1, 38, 38);
+}
+
+function drawBlock(blockType, position, colour){
+  blockType.forEach(function (row,rowIndex){
     row.forEach(function(item,index){
       if (item==1){
         fillBox(rowIndex+position[0],index+position[1],colour)
       }
     })
   })
-
 }
 
-drawBlock(jBlock,[5,-1] ,'red')
-drawBlock(jBlock,[5,-1],'white')
+function moveCurrentBlockDown() {
+  drawBlock(currentBlock.shape, currentBlock.position, 'white')
+  currentBlock.position = [currentBlock.position[0]+1, currentBlock.position[1]]
+  drawBlock(currentBlock.shape, currentBlock.position, currentBlock.colour)
+}
 
-// const blockPositions=[
-//   {
-//     shape: iblock, 
-//     position: [3,3]
-
-//   }
-// ]
-
-//like a dictionary (creating objects)
-const currentBlock=
-  {
-    shape: jBlock, 
-    position: [3,3],
-    colour:'red',
-
+function handleKeyboardEvent(event) {
+  if (event.key === "ArrowUp") {
+    event.preventDefault()
+    console.log("Up arrow pressed")
+  } else if(event.key === "ArrowDown"){
+    event.preventDefault()
+    console.log("Down arrow pressed")
+  } else if(event.key === "ArrowLeft"){
+    event.preventDefault()
+    console.log("Left arrow pressed")
+  } else if(event.key === "ArrowRight"){
+    event.preventDefault()
+    console.log("Right arrow pressed")
   }
-
-drawBlock(currentBlock.shape,currentBlock.position,currentBlock.colour)
-
-setInterval(() => {
-  drawBlock(currentBlock.shape,currentBlock.position,'white')
-  currentBlock.position=[currentBlock.position[0]+1,currentBlock.position[1]]
-  drawBlock(currentBlock.shape,currentBlock.position,currentBlock.colour)
-}, 1000);
-
+}
